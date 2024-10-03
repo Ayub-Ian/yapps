f1f22e977e576f30d507aa1351318e32b28a138e
Ayub Ian <iayub.mia@gmail.com>
2024-10-03 23:22:34 +0300

    initial

---
+++ client/web-app/src/app/[countryCode]/(main)/[handle]/menu/[id]/page.js
@@ -0,0 +1,11 @@
+async function getMenuItemsById(id) {
+  const res = await fetch("");
+}
+
+export default async function MenuItemPage() {
+  return (
+    <>
+      <p>Brunch</p>
+    </>
+  );
+}
